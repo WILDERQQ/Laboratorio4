84cb6a6711293d31c93bffb7d29cb49894aad8c4
SergiiUMSA <152820515+sergiiUMSA@users.noreply.github.com>
2025-03-12 12:00:44 -0400

    Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -1,19 +1,16 @@
 
-import java.util.Scanner; // Importar la clase Scanner
-
+import java.util.Scanner; 
 public class Saludo {
     public static void main(String[] args) {
-        // Crear un objeto Scanner para leer la entrada del usuario
+       
         Scanner teclado = new Scanner(System.in);
      
-        // Pedir el nombre del usuario
+      
         System.out.print("Ingresa tu nombre: ");
-        String nombre = teclado.nextLine(); // Leer el nombre
-     
-        // Mostrar el saludo
+        String nombre = teclado.nextLine(); 
+            
         System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
      
-        // Cerrar el Scanner
         teclado.close();
     }
 }
